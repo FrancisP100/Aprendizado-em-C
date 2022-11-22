5a9ac756715f592635b50aa1b17027a461841b82
FrancisP100 Dev <72936526+FrancisP100@users.noreply.github.com>
2022-11-22 08:57:22 +0100

    Na moral,faço a mínima do que isso faz 😂

---
+++ #004.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main()
+{
+    char ch1;
+    printf("Introduza uma letra: ");
+    scanf("%c", &ch1);
+    printf("A letra '%c' tem o seu número ASCII como %d ", ch1, ch1);
+    
+}
